1f370e20f373f738c57ee8ff015017999d9b239e
FelisterPaul <felisterpaul4@gmail.com>
2021-01-04 21:36:57 +0300

    There You Go

---
+++ gettoknowme.py
@@ -0,0 +1,12 @@
+#print('I AM FELISTER PAUL')
+name = "Felister Paul"
+age = 28
+job_description = "Software Tester"
+print("Name: " + name)
+print("Age: " + str(age))
+print(f'Job Description: {job_description}')
+
+# a = 3 #3 is an int
+# b = "hello" #b IS A STRING
+
+# print(f'any stirng goes here: {a}')
